0586eb4ee8b6f64822a7194e0bc26ad5575662d1
andrey <dryunkaaa@gmail.com>
2025-11-06 17:43:18 +0200

    Add user service with 'hello world' functionality

---
+++ users-service/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("java")
+}
+
+group = "com.dryunkaaa"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+//    implementation("org.postgresql:postgresql")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
